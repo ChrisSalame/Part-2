213f6499e30d9d004edb1d47a2f394620242836e
Christian Salame <salamec@shernet.sheridancollege.ca>
2024-01-31 11:58:06 -0500

    Added  week 2 package while working on week 4 task 3

---
+++ Assets/Week 4/Scripts/PlaneSpawner.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor;
+using UnityEngine;
+
+public class PlaneSpawner : MonoBehaviour
+{
+    public float timer = 0;
+    public GameObject PlanePrefab;
+    public Transform spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < 5.0)
+        {
+            timer = Time.deltaTime;
+            print(timer);
+            
+            
+        }
+
+        if (timer >= 5.0) 
+        {
+            timer = 0;
+            Instantiate(PlanePrefab, spawn.position, spawn.rotation);
+        }
+
+
+    }
+}
