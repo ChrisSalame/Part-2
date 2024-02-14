8c5f2db8d134dc069dd50fb77bcd9b18226bc412
Christian Salame <salamec@shernet.sheridancollege.ca>
2024-02-14 10:21:44 -0500

    Completed Task 1. Scenes change from Week 6 to Week 5 back and forth using a button

---
+++ Assets/Week 6/Scripts/SceneNameText.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class SceneNameText : MonoBehaviour
+{
+    TextMeshProUGUI sceneName;
+    void Start()
+    {
+        sceneName = GetComponent<TextMeshProUGUI>();
+        sceneName.text = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
