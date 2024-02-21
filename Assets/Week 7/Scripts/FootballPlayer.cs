cb8cb9bf8876e4d2572e5b0495b972b01cd3a774
Christian Salame <salamec@shernet.sheridancollege.ca>
2024-02-21 11:10:53 -0500

    Completed Task 1 and Task 2

---
+++ Assets/Week 7/Scripts/FootballPlayer.cs
@@ -0,0 +1,58 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FootballPlayer : MonoBehaviour
+{
+    public Color selectedColor;
+    public Color defaultColor;
+    public SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
+    public float speed = 100;
+
+
+
+    void Start()
+    {
+
+       spriteRenderer = GetComponent<SpriteRenderer>();
+        Selected(false);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown() 
+    {
+        Controller.SetSelectedPlayer(this);
+    }
+
+
+
+    public void Selected(bool isSelected) 
+    { 
+        if (isSelected)
+        {
+            spriteRenderer.color = selectedColor;
+        }
+
+        else
+
+        {
+            spriteRenderer.color = defaultColor;
+        }
+
+    }
+
+       public void Move(Vector2 direction)
+    {
+        rb.AddForce(direction * speed, ForceMode2D.Impulse);
+    }
+
+}
